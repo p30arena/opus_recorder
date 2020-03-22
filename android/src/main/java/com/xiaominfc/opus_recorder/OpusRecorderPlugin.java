d38966dc1d1661e1eef770ba7ac67e7f29e68a06
Ali EP <p30arena@gmail.com>
2020-03-22 22:42:05 +0430

    migrate to androidx

--- android/src/main/java/com/xiaominfc/opus_recorder/OpusRecorderPlugin.java
+++ android/src/main/java/com/xiaominfc/opus_recorder/OpusRecorderPlugin.java
@@ -5,8 +5,8 @@ import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.media.AudioManager;
 import android.os.Build;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import android.util.Log;
 
 import java.util.ArrayList;
